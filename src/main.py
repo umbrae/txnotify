d799cccc562d3ec17aa26613aa866a6d51b0d321
Chris Dary <umbrae@gmail.com>
2012-05-10 10:12:21 -0400

    Cleanup

--- src/main.py
+++ src/main.py
@@ -2,12 +2,11 @@ import settings
 import sys
 import shelve
 import os
-from datetime import datetime
-
-from smtplib import SMTP
-from email.mime.text import MIMEText
 
 from cStringIO import StringIO
+from datetime import datetime
+from email.mime.text import MIMEText
+from smtplib import SMTP
 
 # Import fixofx, uses some trickery to put itself in the path.
 # For more info see: https://github.com/wesabe/fixofx
@@ -19,15 +18,18 @@ def dbg(s):
     if settings.DEBUG:
         print(s)
 
+"""
+ The cache is used to store the last transaction from the last time
+ this script ran. We store it so we don't email duplicate information.
+"""
 cache = shelve.open(os.path.join(settings.TMP_DIR, 'txnotify.cache'))
-def get_last_transaction_id():
-    return cache.get('last_transaction_id')
 
-def set_last_transaction_id(transaction_id):
-    cache['last_transaction_id'] = transaction_id
-    cache.sync()
 
 def parse_line_item(line_item):
+    """ Given an OFX line item, transform it into a friendlier dict of the
+        data we need.
+
+    """
     date_posted_str = line_item.get('DTPOSTED', '')
     date_posted = datetime.strptime(date_posted_str, "%Y%m%d%H%M%S.000")
     return {
@@ -39,7 +41,45 @@ def parse_line_item(line_item):
         "id": line_item.get('FITID')
     }
 
-def send_email(body, subject):
+def generate_email_body(line_items):
+    """ Given a list of line items from OFX, parse the ones that have occurred
+        since the last time we ran and generate an email body of the form:
+
+        [YYYY-MM-DD] {Transaction-name-32-chr-spaced} {Amount}
+        [YYYY-MM-DD] {Transaction-name-32-chr-spaced} {Amount}
+        [YYYY-MM-DD] {Transaction-name-32-chr-spaced} {Amount}
+
+    """
+    email_body = StringIO()
+    last_transaction_id = cache.get('last_transaction_id')
+
+    first = True
+    for line_item in line_items:
+        line_item_dict = line_item.asDict()
+        if line_item_dict.has_key('NAME'):
+            item = parse_line_item(line_item_dict)
+
+            if first:
+                cache['last_transaction_id'] = item['id']
+                cache.sync()
+                first = False
+
+            if item['id'] == last_transaction_id:
+                dbg("Item matched last transaction ID. Breaking.")
+                break
+
+            email_body.write("[%s] %s %s\n" % (
+                item['date_posted'].date().isoformat(),
+                item['name'].ljust(32),
+                item['amount']
+            ))
+        else:
+            dbg("Skipping line item row: %s" % line_item)
+
+    return email_body.getvalue()
+
+
+def send_email(subject, body):
     if len(body) > 0:
         dbg("Sending email with email_body: %s" % body)
 
@@ -56,35 +96,6 @@ def send_email(body, subject):
     else:
         dbg("Body was empty, no records must have been found. Not emailing.")
 
-def generate_email_body(line_items):
-    email_body = StringIO()
-
-    last_transaction_id = get_last_transaction_id()
-
-    first = True
-    for line_item in line_items:
-        line_item_dict = line_item.asDict()
-        if line_item_dict.has_key('NAME'):
-            item = parse_line_item(line_item_dict)
-        
-            if first:
-                set_last_transaction_id(item['id'])
-                first = False
-
-            if item['id'] == last_transaction_id:
-                dbg("Item matched last transaction ID. Breaking.")
-                break
-            
-            email_body.write("[%s] %s %s\n" % (
-                item['date_posted'].date().isoformat(),
-                item['name'].ljust(20),
-                item['amount']
-            ))
-        else:
-            dbg("Skipping line item row: %s" % line_item)
-
-    return email_body.getvalue()
-
 def main():
     # Set up our OFX client
     institution = ofx.Institution(ofx_org=settings.OFX_SETTINGS['org'],
@@ -97,19 +108,21 @@ def main():
     client = ofx.Client(debug=settings.DEBUG)
 
     # Get our current statement
-    response = client.get_statement(account, settings.ACCOUNT_USERNAME,
+    response = client.get_statement(account,
+                                    settings.ACCOUNT_USERNAME,
                                     settings.ACCOUNT_PASSWORD)
 
     # This ugly bit of navigation gets us the list of the transactions that
     # have occured in this statement.
-    line_items = response.get_statements()[0].as_dict()['STMTRS']['BANKTRANLIST']
+    statement_dict = response.get_statements()[0].as_dict()
+    line_items = statement_dict['STMTRS']['BANKTRANLIST']
 
     email_body = generate_email_body(line_items)
 
     now = datetime.now().strftime('%Y-%m-%d %I:%M%p')
     subject = "[%s] New %s Transactions" % (now, settings.ACCOUNT_NAME)
 
-    send_email(email_body, subject)
+    send_email(subject, email_body)
 
 if __name__ == "__main__":
     main()
