f3ae0e5375d10d61404bd7116609b9e19d06b32b
Chris Dary <umbrae@gmail.com>
2012-05-10 09:27:11 -0400

    Cleanup

--- src/main.py
+++ src/main.py
@@ -9,12 +9,15 @@ from email.mime.text import MIMEText
 
 from cStringIO import StringIO
 
-from dateutil.parser import parse as dup
-
-sys.path.insert(0, 'fixofx/3rdparty')
-sys.path.insert(0, 'fixofx/lib')
-
+# Import fixofx, uses some trickery to put itself in the path.
+# For more info see: https://github.com/wesabe/fixofx
+sys.path.insert(0, os.path.join(settings.SRC_DIR, 'fixofx', '3rdparty'))
+sys.path.insert(0, os.path.join(settings.SRC_DIR, 'fixofx', 'lib'))
 import ofx
+
+def dbg(s):
+    if settings.DEBUG:
+        print(s)
 
 cache = shelve.open(os.path.join(settings.TMP_DIR, 'txnotify.cache'))
 def get_last_transaction_id():
@@ -25,7 +28,8 @@ def set_last_transaction_id(transaction_id):
     cache.sync()
 
 def parse_line_item(line_item):
-    date_posted = dup(line_item.get('DTPOSTED', '').replace('.000', ''))
+    date_posted_str = line_item.get('DTPOSTED', '')
+    date_posted = datetime.strptime(date_posted_str, "%Y%m%d%H%M%S.000")
     return {
         "name": line_item.get('NAME'),
         "date_posted": date_posted,
@@ -35,66 +39,78 @@ def parse_line_item(line_item):
         "id": line_item.get('FITID')
     }
 
-institution = ofx.Institution(ofx_org=settings.OFX_SETTINGS['org'],
-                              ofx_url=settings.OFX_SETTINGS['url'],
-                              ofx_fid=settings.OFX_SETTINGS['fid'])
+def send_email(body, subject):
+    if len(body) > 0:
+        dbg("Sending email with email_body: %s" % body)
 
-account = ofx.Account(acct_type=settings.ACCOUNT_TYPE, 
-                      acct_number=settings.ACCOUNT_NUMBER,
-                      aba_number=settings.OFX_SETTINGS['code'], 
-                      institution=institution)
+        msg = MIMEText(body)
+        msg['Subject'] = subject
+        msg['From'] = settings.MAILGUN_API_EMAIL
+        msg['To'] = settings.EMAIL_TO
 
-client = ofx.Client(debug=settings.DEBUG)
-
-last_transaction_id = get_last_transaction_id()
-if not last_transaction_id:
-    print "Last_transaction_id was null."
-
-response = client.get_statement(account, settings.ACCOUNT_USERNAME, settings.ACCOUNT_PASSWORD)
-
-line_items = response.get_statements()[0].as_dict()['STMTRS']['BANKTRANLIST']
-
-email_body = StringIO()
-
-first = True
-for line_item in line_items:
-    line_item_dict = line_item.asDict()
-    if line_item_dict.has_key('NAME'):
-        item = parse_line_item(line_item_dict)
-        
-        if first:
-            set_last_transaction_id(item['id'])
-            first = False
-
-        if item['id'] == last_transaction_id:
-            print "Item matched last transaction ID. Breaking."
-            break
-            
-        email_body.write("[%s] %s %s\n" % (
-            item['date_posted'].date().isoformat(),
-            item['name'].ljust(20),
-            item['amount']
-        ))
-
+        smtp = SMTP("smtp.mailgun.org", 587)
+        smtp.login(settings.MAILGUN_SMTP_USERNAME, settings.MAILGUN_SMTP_PASSWORD)
+        smtp.sendmail(settings.MAILGUN_API_EMAIL,
+                      [settings.EMAIL_TO],
+                      msg.as_string())
     else:
-        print "Skipping line item row: %s" % line_item
+        dbg("Body was empty, no records must have been found. Not emailing.")
 
-now = datetime.now().strftime('%Y-%m-%d %I:%M%p')
-subject = "[%s] New Bank Transactions" % now
-body = email_body.getvalue()
+def generate_email_body(line_items):
+    email_body = StringIO()
 
-if len(body) > 0:
-    print "Sending email with body: %s" % body
+    last_transaction_id = get_last_transaction_id()
 
-    msg = MIMEText(body)
-    msg['Subject'] = subject
-    msg['From'] = settings.MAILGUN_API_EMAIL
-    msg['To'] = settings.EMAIL_TO
-    
-    smtp = SMTP("smtp.mailgun.org", 587)
-    smtp.login(settings.MAILGUN_SMTP_USERNAME, settings.MAILGUN_SMTP_PASSWORD)
-    smtp.sendmail(settings.MAILGUN_API_EMAIL,
-                  [settings.EMAIL_TO],
-                  msg.as_string())
-else:
-    print "No records found, skipping sending email."
+    first = True
+    for line_item in line_items:
+        line_item_dict = line_item.asDict()
+        if line_item_dict.has_key('NAME'):
+            item = parse_line_item(line_item_dict)
+        
+            if first:
+                set_last_transaction_id(item['id'])
+                first = False
+
+            if item['id'] == last_transaction_id:
+                dbg("Item matched last transaction ID. Breaking.")
+                break
+            
+            email_body.write("[%s] %s %s\n" % (
+                item['date_posted'].date().isoformat(),
+                item['name'].ljust(20),
+                item['amount']
+            ))
+        else:
+            dbg("Skipping line item row: %s" % line_item)
+
+    return email_body.getvalue()
+
+def main():
+    # Set up our OFX client
+    institution = ofx.Institution(ofx_org=settings.OFX_SETTINGS['org'],
+                                  ofx_url=settings.OFX_SETTINGS['url'],
+                                  ofx_fid=settings.OFX_SETTINGS['fid'])
+    account = ofx.Account(acct_type=settings.ACCOUNT_TYPE, 
+                          acct_number=settings.ACCOUNT_NUMBER,
+                          aba_number=settings.OFX_SETTINGS['code'], 
+                          institution=institution)
+    client = ofx.Client(debug=settings.DEBUG)
+
+    # Get our current statement
+    response = client.get_statement(account, settings.ACCOUNT_USERNAME,
+                                    settings.ACCOUNT_PASSWORD)
+
+    # This ugly bit of navigation gets us the list of the transactions that
+    # have occured in this statement.
+    line_items = response.get_statements()[0].as_dict()['STMTRS']['BANKTRANLIST']
+
+    email_body = generate_email_body(line_items)
+
+    now = datetime.now().strftime('%Y-%m-%d %I:%M%p')
+    subject = "[%s] New %s Transactions" % (now, settings.ACCOUNT_NAME)
+
+    send_email(email_body, subject)
+
+if __name__ == "__main__":
+    main()
+
--- src/settings.template.py
+++ src/settings.template.py
@@ -2,8 +2,8 @@ import os
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TMP_DIR = os.path.join(PROJECT_ROOT, 'tmp')
+SRC_DIR = os.path.join(PROJECT_ROOT, 'src')
 
-# Doesn't do anything just yet.
 DEBUG = True
 
 # OFX is the protocol used to talk to your bank and get transaction
@@ -16,6 +16,7 @@ OFX_SETTINGS = {
     "code": ""
 }
 
+ACCOUNT_NAME = "Bank"     # Change this to a unique label for your emails.
 ACCOUNT_NUMBER = ""       # The account number at your bank
 ACCOUNT_TYPE = "CHECKING" # Either CHECKING or SAVINGS
 
@@ -23,8 +24,8 @@ ACCOUNT_TYPE = "CHECKING" # Either CHECKING or SAVINGS
 # most secure of boxes. Please ensure that file permissions are set properly
 # and that this is on a machine that you trust completely.
 # 
-# As a good test: Would you run install Quicken with passwordless access on
-# this machine?
+# ANYONE WHO SEES THIS FILE HAS FULL ACCESS TO YOUR ONLINE BANK.
+# 
 ACCOUNT_USERNAME = "" # The username at your bank for online banking
 ACCOUNT_PASSWORD = "" # The password at your bank for online banking
 
